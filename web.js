f3f91cb2f5d57efe80c1587525f33c82b6e515a6
Sergaloh@gmail.com <sergaloh@yahoo.es>
2013-07-17 19:20:39 +0000

    Modifico web.js para que lea de index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+
 app.get('/', function(request, response) {
-  response.send('Hola Mundo') ;
+  //response.send('Hola Mundo!') ;
+    response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
